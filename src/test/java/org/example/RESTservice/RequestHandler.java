82684a9a93ced357b97144b0cacaa39ff3d0420f
Martin-d-Wojcik <martin.d.wojcik@gmail.com>
2023-12-08 10:49:35 +0100

    Moved request calls to RequestHandler class

---
+++ src/test/java/org/example/RESTservice/RequestHandler.java
@@ -0,0 +1,54 @@
+package org.example.RESTservice;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.RequestOptions;
+import org.example.model.ActorModel;
+
+public class RequestHandler {
+    static Playwright playwright;
+    static Browser browser;
+    static BrowserContext browserContext;
+    private static APIRequestContext apiRequestContext;
+
+    public RequestHandler() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    private static void initialiseContext() {
+        browserContext = browser.newContext();
+        browserContext.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true));
+        apiRequestContext = browserContext.request();
+    }
+
+    public APIResponse postRequestActor(String url, ActorModel actorModel) {
+        initialiseContext();
+
+        return apiRequestContext.post(url,
+                RequestOptions.create()
+                        .setData(actorModel));
+    }
+
+    public APIResponse putRequestActor(String url, ActorModel actorModel) {
+        initialiseContext();
+
+        return apiRequestContext.put(url,
+                RequestOptions.create()
+                        .setData(actorModel));
+    }
+
+    public APIResponse deleteRequest(String url) {
+        initialiseContext();
+
+        return apiRequestContext.delete(url);
+    }
+
+    public APIResponse getRequest(String url) {
+        initialiseContext();
+
+        return apiRequestContext.get(url);
+    }
+}
--- src/test/java/org/example/test/ActorApiTest.java
+++ src/test/java/org/example/test/ActorApiTest.java
@@ -2,81 +2,43 @@ package org.example.test;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.RequestOptions;
+import org.example.RESTservice.RequestHandler;
 import org.example.RESTservice.ResponseHandler;
 import org.example.model.ActorModel;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ActorApiTest {
-    static Playwright playwright;
-    static Browser browser;
-    static BrowserContext browserContext;
+    // static Playwright playwright;
+    // static Browser browser;
+    // static BrowserContext browserContext;
     private static String baseUrl;
     private static APIRequestContext apiRequestContext;
     private static APIResponse response;
     private static ActorModel actorModel;
+    private static RequestHandler requestHandler;
 
     @BeforeAll
     static void setup() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch();
+        // playwright = Playwright.create();
+        // browser = playwright.chromium().launch();
+
+        requestHandler = new RequestHandler();
         baseUrl = "http:/localhost:8080/api/v1";
-    }
-
-    @BeforeEach
-    public void initialiseContext() {
-        //Below lines of code will enable tracing
-        browserContext = browser.newContext();
-        browserContext.tracing().start(new Tracing.StartOptions()
-                .setScreenshots(true)
-                .setSnapshots(true)
-                .setSources(true));
-    }
-
-    @Test
-    public void testGetActorByIdShouldPass() {
-        // Prepare
-        apiRequestContext = browserContext.request();
-        long actorId = 2L;
-
-        // Perform
-        response = apiRequestContext.get(baseUrl + "/actor/id/" + actorId);
-
-        // Assert
-        assertEquals(200, response.status());
-
-        // Deserialise response to actor object
-        actorModel = ResponseHandler.deserialiseResponseToActorModelObject(response);
-        assertEquals("Jason", actorModel.getFirstName());
-        assertEquals("Statham", actorModel.getLastName());
-        assertEquals("UK", actorModel.getBirthCountry());
-    }
-
-    @Test
-    public void testGetAllActorsShouldReturnArray() {
-        // Prepare
-        apiRequestContext = browserContext.request();
-
-        // Perform
-        response = apiRequestContext.get(baseUrl + "/actors");
-
-        // Assert
-        assertEquals(200, response.status());
-        assertTrue(ResponseHandler.arrayInResponseHasValues(response));
     }
 
     @Test
     public void testGetActorByNameShouldPass() {
         // Prepare
-        apiRequestContext = browserContext.request();
         String actorFirstName = "Jason";
 
         // Perform
-        response = apiRequestContext.get(baseUrl + "/actor/" + actorFirstName);
+        response = requestHandler.getRequest(baseUrl + "/actor/" + actorFirstName);
 
         // Assert
         assertEquals(200, response.status());
@@ -86,51 +48,76 @@ public class ActorApiTest {
     @Test
     public void testAddNewActorShouldPass() {
         // Prepare
-        apiRequestContext = browserContext.request();
-        actorModel = new ActorModel(100L,
-                "Sylvester",
-                "Stallone",
+        actorModel = new ActorModel(102L,
+                "Lester",
+                "Allone",
                 "USA",
                 1L);
 
         // Perform
-        response = apiRequestContext.post(baseUrl + "/actor",
-                RequestOptions.create()
-                        .setData(actorModel));
+        response = requestHandler.postRequestActor(baseUrl + "/actor", actorModel);
 
         // Assert
         assertEquals(201, response.status());
+        assertTrue(response.text().contains("Created"));
+        assertTrue(response.text().contains(actorModel.getFirstName()));
+        assertTrue(response.text().contains(actorModel.getLastName()));
+    }
+
+    @Test
+    public void testGetActorByIdShouldPass() {
+        // Prepare
+        long actorId = 2L;
+
+        // Perform
+        response = requestHandler.getRequest(baseUrl + "/actor/id/" + actorId);
+
+        // Assert
+        assertEquals(200, response.status());
+        actorModel = ResponseHandler.deserialiseResponseToActorModelObject(response);
+        assertEquals("Jason", actorModel.getFirstName());
+        assertEquals("Statham", actorModel.getLastName());
+        assertEquals("UK", actorModel.getBirthCountry());
+    }
+
+    @Test
+    public void testGetAllActorsShouldReturnArray() {
+        // Perform
+        response = requestHandler.getRequest(baseUrl + "/actors");
+
+        // Assert
+        assertEquals(200, response.status());
+        assertTrue(ResponseHandler.arrayInResponseHasValues(response));
     }
 
     @Test
     public void testUpdateActorShouldPass() {
         // Prepare
-        long actorId = 100L;
-        apiRequestContext = browserContext.request();
-        ActorModel updateActor = new ActorModel("Sly",
-                "Stollane",
+        long actorId = 102L;
+        ActorModel updateActor = new ActorModel(actorId,
+                "Les",
+                "Alone",
                 "USA",
                 1L);
 
         // Perform
-        APIResponse responseUpdated = apiRequestContext.put(baseUrl + "/actor/" + actorId,
-                RequestOptions.create()
-                        .setData(updateActor));
+        response = requestHandler.putRequestActor(baseUrl + "/actor/" + actorId, updateActor);
 
         // Assert
         assertEquals(200, response.status());
+        assertTrue(response.text().contains("Updated"));
     }
 
     @Test
     public void testDeleteActorByIdShouldPass() {
         // Prepare
-        long actorId = 100L;
-        apiRequestContext = browserContext.request();
+        long actorId = 102L;
 
         // Perform
-        response = apiRequestContext.delete(baseUrl + "/actor/" + actorId);
+        response = requestHandler.deleteRequest(baseUrl + "/actor/" + actorId);
 
         // Assert
         assertEquals(200, response.status());
+        assertTrue(response.text().contains("Deleted"));
     }
 }
